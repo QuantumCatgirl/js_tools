c00b5d60cea2aa74ded5853255cbf9440bbad37e
Will Tomlins <will.tomlins@unboxedconsulting.com>
2011-11-04 10:34:56 +0000

    Added event_handler.

---
+++ event_handler.js
@@ -0,0 +1,53 @@
+function EventHandler() {
+  var events = {};
+  var self = this;
+  this.events = events;
+  
+  this.log_events = false;
+  
+  function bind(key, callback) {
+    
+    // handle multiple events: event_handler.bind(['first_event', 'seccond_event'], callback)
+    if (typeof(key) == 'object') {
+      for (var e in key) {
+        events[key[e]] = events[key[e]] || [];
+        events[key[e]].push(callback);
+      }
+    }
+    // handle events identified by strings: event_handler.bind('an_event', callback)
+    else {
+      events[key] = events[key] || [];
+      events[key].push(callback);
+    }
+  };
+  this.bind = bind;
+
+  function unbind(key, callback){
+    for (var i in events[key]) {
+      if (events[key][i] == callback) {
+        events[key].splice(i, 1);
+      }
+    }
+  };
+  this.unbind = unbind;
+
+  function fire() {
+    var args = Utils.arrayify_arguments(arguments);
+    var key = args.shift();
+    
+    if (self.log_events) logger.log(self, key, args);
+    
+    if (!(args instanceof Array)) { args = [args]; }
+    
+    if (events[key] && events[key].length > 0) {
+      for (var i in events[key]) {
+        var func = events[key][i];
+        func.apply(self, args);
+      }
+    }
+  };
+  this.fire = fire;
+};
+
+window.event_handler = new EventHandler();
+
